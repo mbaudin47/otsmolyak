5557e8393ce06be1fac40baae9db54130838191a
Michael BAUDIN <michael.baudin@gmail.com>
2019-05-17 21:53:18 +0200

    Removed cc_int_smolyak which does not exist

--- lib/src/smolpack/otsmolyak/smolpack.hh
+++ lib/src/smolpack/otsmolyak/smolpack.hh
@@ -7,8 +7,7 @@ namespace OTSMOLPACK
 # define maxdim 40
 
 extern double int_smolyak(int, int, double(*ff)(int, double x[]), int);
-extern double cc_int_smolyak(int, int, double(*ff)(int, double x[]), int);
 
-} /* namespace OTSMOLYAK */
+} /* namespace OTSMOLPACK */
 
 #endif /* OTSMOLYAK_SMOLPACK_HXX */
